04c14f9fc3a2b85b7fed565cd9b3d962e51c10e3
F Y <fyemath@hotmail.com>
2021-01-19 15:52:04 -0500

    updated css

--- static/js/article-meta-height.js
+++ static/js/article-meta-height.js
@@ -1,7 +1,9 @@
-$(document).ready(getHeight);
-$(window).on('resize', getHeight);
+$(document).ready(getDimension);
+$(window).on('resize', getDimension);
 
-function getHeight() {
+function getDimension() {
   var height = $(".article-meta h1").outerHeight(true) + $(".article-meta h2").outerHeight(true) + $(".article-meta h2").innerHeight()  + "px";
   document.documentElement.style.setProperty('--bg-height', height);
+  // var width = $("main").width()  + "px";
+  // document.documentElement.style.setProperty('--bg-width', width);
 }
