83aa0c789c78ae2eeaee37b898c4a32bc9bd808b
F Y <fyemath@gmail.com>
2021-04-24 13:45:22 -0400

    removed public folder

--- public/js/article-meta-height.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(getDimension);
-$(window).on('resize', getDimension);
-
-function getDimension() {
-  var height = $(".article-meta h1").outerHeight(true) + $(".article-meta h2").outerHeight(true) + $(".article-meta h2").innerHeight()  + "px";
-  document.documentElement.style.setProperty('--bg-height', height);
-  // var width = $("main").width()  + "px";
-  // document.documentElement.style.setProperty('--bg-width', width);
-}
